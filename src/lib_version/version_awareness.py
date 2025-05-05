45021d1301fc2ab8c0003b66c3fd42b94ef04556
Koen Bron <koenbron2002@gmail.com>
2025-05-05 17:33:39 +0200

    patch: removed legacy code and updated the workflow to automatically push to main

--- src/lib_version/version_awareness.py
+++ src/lib_version/version_awareness.py
@@ -3,8 +3,6 @@ from importlib.resources import files
 class VersionUtil():
     def __init__(self):
         try:
-            # version_data = pkg_resources.resource_string('lib-version', 'VERSION')
-            # return version_data.decode("utf-8").strip()
             self.__version__ = files("lib_version").joinpath("VERSION").read_text().strip()
 
         except:
